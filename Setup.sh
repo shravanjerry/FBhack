8192eb2c5497678e923082f1ba1082ae9441eeb0
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-06-17 23:15:58 +0530

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m]\e[0m\e[1;32m !!! MUST ROOT OTHERWISE SCRIPT NOT WORKING PROPERLY !!! \e[0m'
+sleep 3
+clear
+echo ""
+echo ""
+echo -e $'\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m]\e[0m\e[1;32m >> DOWNLOAD REQUIREMENTS >> \e[0m'
+sudo apt-get install apache2 -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+systemctl start apache2
+apt install xterm -y
+apt install gnome-terminal -y
+unzip ngrok-stable-linux-amd64.zip
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+cp -R ngrok sites/facebook/
+chmod 7777 phishing.sh
+clear
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m]\e[0m\e[1;32m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+clear
+
+
+
